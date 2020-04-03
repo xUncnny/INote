840b9618db8077435255973d3f8b0eebfea7460d
xUncnny <62380601+xUncnny@users.noreply.github.com>
2020-04-04 02:20:31 +0300

    Update settings.py

--- noteitnow/settings.py
+++ noteitnow/settings.py
@@ -65,12 +65,9 @@ WSGI_APPLICATION = 'noteitnow.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 
 
